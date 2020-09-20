371b30bf59ca02a9bb191bd9f88e3f2a8ecefb07
John Baxter <jbaxter@a-zdarts.com>
2020-09-19 19:31:41 -0700

    Made file read and write async

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const crypto = require("crypto");
+const util = require("util");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 
 const app = express();
@@ -15,43 +19,43 @@ app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, function() {
-    console.log("App listening at localhots:" + PORT);
+    console.log("App listening at http://localhost:" + PORT);
   });
 
 app.get("/notes", (req,res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
-app.get("/api/notes", (req,res) => {
-    let rawData = fs.readFileSync(path.join(__dirname, "db/db.json"));
+app.get("/api/notes", async (req,res) => {
+    let rawData = await readFileAsync(path.join(__dirname, "db/db.json"));
     let notesJson = JSON.parse(rawData);
     return res.json(notesJson);
 });
 
-app.post("/api/notes", (req,res) => {
-    let rawData = fs.readFileSync(path.join(__dirname, "db/db.json"));
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.post("/api/notes", async (req,res) => {
+    let rawData = await readFileAsync(path.join(__dirname, "db/db.json"));
     console.log(req.body);
     let notesJson = JSON.parse(rawData);
-    req.body.id = crypto.randomBytes(16).toString("hex");
+    req.body.id = crypto.randomBytes(10).toString("hex");
     notesJson.push(req.body);
-    fs.writeFileSync(path.join(__dirname, "db/db.json"),JSON.stringify( notesJson));
+    await writeFileAsync(path.join(__dirname, "db/db.json"),JSON.stringify( notesJson));
     
     return res.send(notesJson);
 });
 
-app.delete("/api/notes/:id", (req,res) => {
+app.delete("/api/notes/:id", async (req,res) => {
     var chosen = req.params.id;
-    let rawData = fs.readFileSync(path.join(__dirname, "db/db.json"));
+    let rawData = await readFileAsync(path.join(__dirname, "db/db.json"));
     let notesJson = JSON.parse(rawData);
     var removeIndex = notesJson.map(item => item.id)
                        .indexOf(chosen);
     console.log(removeIndex);
     ~removeIndex && notesJson.splice(removeIndex, 1);
-    fs.writeFileSync(path.join(__dirname, "db/db.json"),JSON.stringify( notesJson));
+    await writeFileAsync(path.join(__dirname, "db/db.json"),JSON.stringify( notesJson));
 
     return res.send(notesJson);
 });
